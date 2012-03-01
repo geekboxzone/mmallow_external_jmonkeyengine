59b2e6871c65f58fdad78cd7229c292f6a177578
Scott Barta <sbarta@google.com>
2012-03-01 12:35:35 -0800

    Adds the jMonkeyEngine library to the build.
    Adds the jMonkeyEngine open source 3D game engine to the build. This
    is built as a static library and is only used by the Finsky client.
    
    Change-Id: I06a3f054df7b8a67757267d884854f70c5a16ca0

---
+++ engine/src/android/com/jme3/audio/plugins/AndroidAudioLoader.java
@@ -0,0 +1,19 @@
+package com.jme3.audio.plugins;
+
+import com.jme3.asset.AssetInfo;
+import com.jme3.asset.AssetLoader;
+import com.jme3.audio.android.AndroidAudioData;
+import java.io.IOException;
+
+public class AndroidAudioLoader implements AssetLoader 
+{
+
+    @Override
+    public Object load(AssetInfo assetInfo) throws IOException 
+    {
+        AndroidAudioData result = new AndroidAudioData();
+        result.setAssetKey( assetInfo.getKey() );
+        return result;
+    }
+
+}
